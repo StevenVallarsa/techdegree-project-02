d0917d29fa9627805dadad9eb93a04deebcc13b0
StevenVallarsa <orionsjv@gmail.com>
2022-06-17 18:15:43 -0400

    Completed basic search functionality

--- js/script.js
+++ js/script.js
@@ -10,43 +10,42 @@ For assistance:
 */
 
 /**
- * Add Search Bar
+ * Search Bar
  */
 const searchBar = `
-  <form id="search-form">
     <label for="search" class="student-search">
       <span>Search by name</span>
       <input id="search" placeholder="Search by name...">
       <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
-    </label>
-  </form>`;
+    </label>`;
 const header = document.querySelector("header");
 header.insertAdjacentHTML("beforeend", searchBar);
 
-const searchForm = document.querySelector("#search-form");
 const input = document.querySelector("#search");
 const searchButton = document.querySelector("button");
 
-input.addEventListener("keydown", e => {
-  const searchTerm = e.target.value.toLowerCase();
+// Filter search term and display results
+function searchResults(searchTerm) {
   const results = data.filter(item => {
     const name = `${item.name.first.toLowerCase()} ${item.name.last.toLowerCase()}`;
     return name.includes(searchTerm);
   });
-
   showPage(results, 1);
   addPagination(results);
+}
+
+// Live search results
+input.addEventListener("keyup", e => {
+  const searchTerm = e.target.value.toLowerCase();
+  searchResults(searchTerm);
 });
 
-// searchButton.addEventListener("click", e => {
-//   const searchTerm = input.value.toLowerCase();
-//   const results = data.filter(item => {
-//     const name = `${item.name.first.toLowerCase()} ${item.name.last.toLowerCase()}`;
-//     return name.includes(searchTerm);
-//   });
-//   showPage(results, 1);
-//   addPagination(results);
-// });
+// Button push search results
+searchButton.addEventListener("click", () => {
+  const searchTerm = input.value.toLowerCase();
+  input.value = "";
+  searchResults(searchTerm);
+});
 
 /*
 Create the `showPage` function
@@ -95,36 +94,38 @@ function addPagination(list) {
   const ul = document.querySelector(".link-list");
   ul.innerHTML = ""; // empty UL area of existing items
 
-  // Create and insert number buttons
-  for (let i = 0; i < list.length / cardsPerPage; i++) {
-    const button = `
-     <li>
-        <button type="button">${i + 1}</button>
-     </li>
-    `;
-    ul.insertAdjacentHTML("beforeend", button);
-  }
-
-  // Make first number button "active"
-  ul.firstElementChild.firstElementChild.className = "active";
-
-  // Handle button click events
-  ul.addEventListener("click", e => {
-    if (e.target.tagName === "BUTTON") {
-      // If "button" clicked make clicked button active
-      // (while looping to find clicked button, remove "active" class)
-      const pageButtons = Array.from(document.querySelectorAll(".link-list li"));
-      pageButtons.forEach(button => {
-        button.firstElementChild.className = "";
-        if (e.target.textContent === button.firstElementChild.textContent) {
-          button.firstElementChild.className = "active";
-        }
-      });
-
-      // Display "showPage" with new page number
-      showPage(data, e.target.textContent);
+  if (list.length !== 0) {
+    // Create and insert number buttons
+    for (let i = 0; i < list.length / cardsPerPage; i++) {
+      const button = `
+      <li>
+      <button type="button">${i + 1}</button>
+      </li>
+      `;
+      ul.insertAdjacentHTML("beforeend", button);
     }
-  });
+
+    // Make first number button "active"
+    ul.firstElementChild.firstElementChild.className = "active";
+
+    // Handle button click events
+    ul.addEventListener("click", e => {
+      if (e.target.tagName === "BUTTON") {
+        // If "button" clicked make clicked button active
+        // (while looping to find clicked button, remove "active" class)
+        const pageButtons = Array.from(document.querySelectorAll(".link-list li"));
+        pageButtons.forEach(button => {
+          button.firstElementChild.className = "";
+          if (e.target.textContent === button.firstElementChild.textContent) {
+            button.firstElementChild.className = "active";
+          }
+        });
+
+        // Display "showPage" with new page number
+        showPage(data, e.target.textContent);
+      }
+    });
+  }
 }
 
 // Call functions
